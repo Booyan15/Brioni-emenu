8a3bd33c241c606fd7e0b35482035a202fd7a2b9
Bojan Popovski <144334411+Booyan15@users.noreply.github.com>
2025-06-23 16:35:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -287,265 +287,264 @@ const menuItems = {
       {
         name: "Portion with 5 kebabs",
         description: "5 kebabs, bread and onions",
-        price: "100 MKD",
+        price: "110 денари",
         category: "skara",
         image: "BPP_3068JPG.jpg"
       },
       {
         name: "Portion with 6 kebabs",
         description: "6 kebabs, bread and onions",
-        price: "120 MKD",
+        price: "130 денари",
         category: "skara",
         image: "BPP_3069JPG.jpg"
       },
       {
         name: "Portion with 7 kebabs",
         description: "7 kebabs, bread and onions",
-        price: "130 MKD",
+        price: "150 денари",
         category: "skara",
         image: "BPP_3072JPG.jpg"
       },
       {
         name: "Portion with 8 kebabs",
         description: "8 kebabs, bread and onions",
-        price: "150 MKD",
+        price: "170 денари",
         category: "skara",
         image: "BPP_3074JPG.jpg"
       },
       {
         name: "Portion with 10 kebabs",
         description: "10 kebabs, bread and onions",
-        price: "180 MKD",
+        price: "200 денари",
         category: "skara",
         image: "BPP_3076JPG.jpg"
       },
       {
         name: "Portion with 15 kebabs",
         description: "15 kebabs, bread and onions",
-        price: "260 MKD",
+        price: "290 денари",
         category: "skara",
         image: "BPP_3084JPG.jpg"
       },
       {
         name: "Portion with 20 kebabs",
         description: "20 kebabs, bread and onions",
-        price: "340 MKD",
+        price: "380 денари",
         category: "skara",
         image: "BPP_3088JPG.jpg"
       },
       {
         name: "Kebab (for home)",
         description: "",
-        price: "16 MKD",
+        price: "18 денари",
         category: "skara",
         image: "BPP_3096JPG.jpg"
       },
       {
         name: "Portion with sausages",
         description: "Sausages, bread and onions",
-        price: "220 MKD",
+        price: "240 денари",
         category: "skara",
         image: "BPP_3089JPG.jpg"
       },
       {
         name: "Portion of sudzhuk",
         description: "Sudzhuk, bread and onions",
-        price: "220 MKD",
+        price: "240 денари",
         category: "skara",
         image: "BPP_3092JPG.jpg"
       },
       {
         name: "Small burger patty",
         description: "",
-        price: "100 MKD",
+        price: "110 денари",
         category: "skara",
         image: "BPP_3098JPG.jpg"
       },
       {
         name: "Large burger patty",
         description: "",
-        price: "180 MKD",
+        price: "200 денари",
         category: "skara",
         image: "BPP_3109JPG.jpg"
       },
       {
         name: "Greek burger portion",
         description: "Greek burger, bread and onions",
-        price: "250 MKD",
+        price: "270 денари",
         category: "skara",
         image: "BPP_3066JPG.jpg"
       },
       {
         name: "Steak portion",
         description: "Steak, bread and onions",
-        price: "200 MKD",
+        price: "220 денари",
         category: "skara",
         image: "BPP_3065JPG.jpg"
       },
       {
         name: "Chicken drumstick portion",
         description: "Drumstick, bread, and onion",
-        price: "200 MKD",
+        price: "220 денари",
         category: "skara",
-        image: "BPP_3093JPG.jpg",
+        image: "BPP_3093JPG.jpg"
       },
       {
         name: "Chicken portion",
         description: "Chicken, bread and onions",
-        price: "180 MKD",
+        price: "200 денари",
         category: "skara",
         image: "BPP_3082JPG.jpg"
       },
       {
         name: "Chicken (for home)",
         description: "Chicken",
-        price: "300 MKD",
+        price: "300 денари",
         category: "skara",
         image: "BPP_3078JPG.jpg"
       },
       {
-        name: "kebab sandwich",
+        name: "Kebab sandwich",
         description: "5 kebabs, bread and salad",
-        price: "100 MKD",
+        price: "110 денари",
         category: "skara",
         image: "BPP_3147JPG.jpg"
       },
       {
         name: "Sandwich with chicken drumstick",
         description: "chicken drumstick, bread and salad",
-        price: "110 MKD",
+        price: "120 денари",
         category: "skara",
         image: "BPP_3148JPG.jpg"
       },
       {
         name: "Kaymak (creamy cheese)",
         description: "Kaymak",
-        price: "50 MKD",
+        price: "60 денари",
         category: "dodatoci",
         image: "BPP_3121JPG.jpg"
       },
       {
         name: "Pepper",
         description: "Roasted pepper",
-        price: "20/30 MKD",
+        price: "20/30 денари",
         category: "dodatoci",
         image: "BPP_3123JPG.jpg"
       },
       {
         name: "Bread",
         description: "",
-        price: "20 MKD",
+        price: "20 денари",
         category: "dodatoci",
         image: "BPP_3141JPG.jpg"
       },
       {
         name: "Onion (salad)",
         description: "Onion with seasoning",
-        price: "30 MKD",
+        price: "30 денари",
         category: "salata",
         image: "BPP_3126JPG.jpg"
       },
       {
         name: "Mixed salad (small)",
         description: "Cucumber, eggplant, cabbage",
-        price: "70 MKD",
+        price: "70 денари",
         category: "salata",
         image: "BPP_3111JPG.jpg"
       },
       {
         name: "Mixed salad (medium)",
         description: "Cucumber, eggplant, cabbage",
-        price: "120 MKD",
+        price: "120 денари",
         category: "salata",
         image: "BPP_3114JPG.jpg"
       },
       {
         name: "Mixed salad (large)",
         description: "Cucumber, eggplant, cabbage",
-        price: "150 MKD",
+        price: "150 денари",
         category: "salata",
         image: "BPP_3119JPG.jpg"
       },
       {
         name: "Mixed salad (large)",
         description: "Cucumber, eggplant, cabbage",
-        price: "200 MKD",
+        price: "200 денари",
         category: "salata",
         image: "BPP_3119JPG.jpg"
       },
       {
         name: "Shopska salad (small)",
         description: "Cucumber, eggplant and cheese",
-        price: "150 MKD",
+        price: "150 денари",
         category: "salata",
         image: "BPP_3101JPG.jpg"
       },
       {
         name: "Shopska salad (large)",
         description: "Cucumber, eggplant and cheese",
-        price: "250 MKD",
+        price: "250 денари",
         category: "salata",
         image: "BPP_3106JPG.jpg"
       },
       {
         name: "Beer",
         description: "Zlaten Dab Amber, Skopsko, Krali Marko, Zlaten Dab, Skopsko Smooth",
-        price: "100 MKD",
+        price: "100 денари",
         category: "pijaloci",
         image: "BPP_3128JPG.jpg"
       },
       {
         name: "Heineken",
         description: "",
-        price: "130 MKD",
+        price: "130 денари",
         category: "pijaloci",
         image: "BPP_3131JPG.jpg"
       },
       {
         name: "Juice",
         description: "Mirinda Lemon, Pepsi, Mirinda Orange, Seven Up, Gazosa, Peach, Apple, Orange",
-        price: "70 MKD",
+        price: "70 денари",
         category: "pijaloci",
         image: "BPP_3133JPG.jpg"
       },
       {
         name: "Water (still)",
         description: "Izvorska, Pelisterka",
-        price: "40 MKD",
+        price: "50 денари",
         category: "pijaloci",
         image: "BPP_3135JPG.jpg"
       },
       {
         name: "Water (sparkling)",
         description: "Pelisterka, Kozuvchanka",
-        price: "40 MKD",
+        price: "50 денари",
         category: "pijaloci",
         image: "BPP_3136JPG.jpg"
       },
       {
         name: "Wine 0.5l",
         description: "Smederivka, Rose",
-        price: "180 MKD",
+        price: "180 денари",
         category: "pijaloci",
         image: "BPP_3138JPG.jpg"
       },
       {
         name: "Rakija",
         description: "Mastika, Grape Rakija, Badel",
-        price: "80 MKD",
+        price: "80 денари",
         category: "pijaloci",
         image: "BPP_3139JPG.jpg"
       },
       {
         name: "Coffee",
         description: "",
-        price: "50 MKD",
+        price: "60 денари",
         category: "pijaloci",
         image: "BPP_3146JPG.jpg"
       }
     ]
   },
-
   tr: {
     hero: {
       title: "Kalıcı Tat",
@@ -561,259 +560,259 @@ const menuItems = {
       {
         name: "5 köfte porsiyon",
         description: "5 köfte, ekmek ve soğan",
-        price: "100 MKD",
+        price: "110 денари",
         category: "skara",
         image: "BPP_3068JPG.jpg"
       },
       {
         name: "6 köfte porsiyon",
         description: "6 köfte, ekmek ve soğan",
-        price: "120 MKD",
+        price: "130 денари",
         category: "skara",
         image: "BPP_3069JPG.jpg"
       },
       {
         name: "7 köfte porsiyon",
         description: "7 köfte, ekmek ve soğan",
-        price: "130 MKD",
+        price: "150 денари",
         category: "skara",
         image: "BPP_3072JPG.jpg"
       },
       {
         name: "8 köfte porsiyon",
         description: "8 köfte, ekmek ve soğan",
-        price: "150 MKD",
+        price: "170 денари",
         category: "skara",
         image: "BPP_3074JPG.jpg"
       },
       {
         name: "10 köfte porsiyon",
         description: "10 köfte, ekmek ve soğan",
-        price: "180 MKD",
+        price: "200 денари",
         category: "skara",
         image: "BPP_3076JPG.jpg"
       },
       {
         name: "15 köfte porsiyon",
         description: "15 köfte, ekmek ve soğan",
-        price: "260 MKD",
+        price: "290 денари",
         category: "skara",
         image: "BPP_3084JPG.jpg"
       },
       {
         name: "20 köfte porsiyon",
         description: "20 köfte, ekmek ve soğan",
-        price: "340 MKD",
+        price: "380 денари",
         category: "skara",
         image: "BPP_3088JPG.jpg"
       },
       {
         name: "Köfte (ev için)",
         description: "",
-        price: "16 MKD",
+        price: "18 денари",
         category: "skara",
         image: "BPP_3096JPG.jpg"
       },
       {
         name: "Sucuk porsiyon",
         description: "Sucuk, ekmek ve soğan",
-        price: "220 MKD",
+        price: "240 денари",
         category: "skara",
         image: "BPP_3089JPG.jpg"
       },
       {
         name: "Sucuk porsiyon",
         description: "Sucuk, ekmek ve soğan",
-        price: "220 MKD",
+        price: "240 денари",
         category: "skara",
         image: "BPP_3092JPG.jpg"
       },
       {
         name: "Küçük köfte",
         description: "",
-        price: "100 MKD",
+        price: "110 денари",
         category: "skara",
         image: "BPP_3098JPG.jpg"
       },
       {
         name: "Büyük köfte",
         description: "",
-        price: "180 MKD",
+        price: "200 денари",
         category: "skara",
         image: "BPP_3109JPG.jpg"
       },
       {
         name: "Yunan köfte porsiyon",
         description: "Yunan köfte, ekmek ve soğan",
-        price: "250 MKD",
+        price: "270 денари",
         category: "skara",
         image: "BPP_3066JPG.jpg"
       },
       {
         name: "Biftek porsiyon",
         description: "Biftek, ekmek ve soğan",
-        price: "200 MKD",
+        price: "220 денари",
         category: "skara",
         image: "BPP_3065JPG.jpg"
       },
       {
         name: "But porsiyon",
         description: "But, ekmek ve soğan",
-        price: "200 MKD",
-        category: "ızgara",
+        price: "220 денари",
+        category: "skara",
         image: "BPP_3093JPG.jpg"
       },
       {
         name: "Tavuk porsiyon",
         description: "Tavuk, ekmek ve soğan",
-        price: "180 MKD",
+        price: "200 денари",
         category: "skara",
         image: "BPP_3082JPG.jpg"
       },
       {
         name: "Tavuk (ev için)",
         description: "Tavuk",
-        price: "300 MKD",
+        price: "300 денари",
         category: "skara",
         image: "BPP_3078JPG.jpg"
       },
       {
         name: "Kebaplı sandviç",
         description: "5 kebap, ekmek ve salata",
-        price: "100 MKD",
+        price: "110 денари",
         category: "skara",
         image: "BPP_3147JPG.jpg"
       },
       {
         name: "tavuk baget sandviç",
         description: "tavuk baget, ekmek ve salata",
-        price: "110 денари",
+        price: "120 денари",
         category: "skara",
         image: "BPP_3148JPG.jpg"
       },
       {
         name: "Kaymak",
         description: "Kaymak",
-        price: "50 MKD",
+        price: "60 денари",
         category: "dodatoci",
         image: "BPP_3121JPG.jpg"
       },
       {
         name: "Biber",
         description: "Közlenmiş biber",
-        price: "20/30 MKD",
+        price: "20/30 денари",
         category: "dodatoci",
         image: "BPP_3123JPG.jpg"
       },
       {
         name: "Ekmek",
         description: "",
-        price: "20 MKD",
+        price: "20 денари",
         category: "dodatoci",
         image: "BPP_3141JPG.jpg"
       },
       {
         name: "Soğan (salata)",
         description: "Baharatlı soğan",
-        price: "30 MKD",
+        price: "30 денари",
         category: "salata",
         image: "BPP_3126JPG.jpg"
       },
       {
         name: "Karışık salata (küçük)",
         description: "Salatalık, patlıcan, lahana",
-        price: "70 MKD",
+        price: "70 денари",
         category: "salata",
         image: "BPP_3111JPG.jpg"
       },
       {
         name: "Karışık salata (orta)",
         description: "Salatalık, patlıcan, lahana",
-        price: "120 MKD",
+        price: "120 денари",
         category: "salata",
         image: "BPP_3114JPG.jpg"
       },
       {
         name: "Karışık salata (büyük)",
         description: "Salatalık, patlıcan, lahana",
-        price: "150 MKD",
+        price: "150 денари",
         category: "salata",
         image: "BPP_3119JPG.jpg"
       },
       {
         name: "Karışık salata (büyük)",
         description: "Salatalık, patlıcan, lahana",
-        price: "200 MKD",
+        price: "200 денари",
         category: "salata",
         image: "BPP_3119JPG.jpg"
       },
       {
         name: "Şopska salata (küçük)",
         description: "Salatalık, patlıcan ve peynir",
-        price: "150 MKD",
+        price: "150 денари",
         category: "salata",
         image: "BPP_3101JPG.jpg"
       },
       {
         name: "Şopska salata (büyük)",
         description: "Salatalık, patlıcan ve peynir",
-        price: "250 MKD",
+        price: "250 денари",
         category: "salata",
         image: "BPP_3106JPG.jpg"
       },
       {
         name: "Bira",
         description: "Zlaten Dab Amber, Skopsko, Krali Marko, Zlaten Dab, Skopsko Smooth",
-        price: "100 MKD",
+        price: "100 денари",
         category: "pijaloci",
         image: "BPP_3128JPG.jpg"
       },
       {
         name: "Heineken",
         description: "",
-        price: "130 MKD",
+        price: "130 денари",
         category: "pijaloci",
         image: "BPP_3131JPG.jpg"
       },
       {
         name: "Meyve suyu",
         description: "Mirinda Limon, Pepsi, Mirinda Portakal, Seven Up, Gazoz, Şeftali, Elma, Portakal",
-        price: "70 MKD",
+        price: "70 денари",
         category: "pijaloci",
         image: "BPP_3133JPG.jpg"
       },
       {
         name: "Su (durgun)",
         description: "Pelisterka, Izvorska",
-        price: "40 MKD",
+        price: "50 денари",
         category: "pijaloci",
         image: "BPP_3135JPG.jpg"
       },
       {
         name: "Su (gazlı)",
         description: "Pelisterka, Kozuvchanka",
-        price: "40 MKD",
+        price: "50 денари",
         category: "pijaloci",
         image: "BPP_3136JPG.jpg"
       },
       {
         name: "Şarap 0.5l",
         description: "Smederevka, Rose",
-        price: "180 MKD",
+        price: "180 денари",
         category: "pijaloci",
         image: "BPP_3138JPG.jpg"
       },
       {
         name: "Rakija 50ml",
         description: "Mastika, Üzüm Rakısı, Badel",
-        price: "80 MKD",
+        price: "80 денари",
         category: "pijaloci",
         image: "BPP_3139JPG.jpg"
       },
       {
         name: "Kahve",
         description: "",
-        price: "50 MKD",
+        price: "60 денари",
         category: "pijaloci",
         image: "BPP_3146JPG.jpg"
       }
